e1a6be3617ab684dbdb396459ef0e99703d0d588
miampf <miampf@proton.me>
2023-03-31 17:54:53 +0200

    added obligatory comments

--- lua/autocommands.lua
+++ lua/autocommands.lua
@@ -1,9 +1,13 @@
+-- automatically recompile packer plugins on saving plugins.lua
 vim.cmd([[
   augroup packer_user_config
     autocmd!
     autocmd BufWritePost plugins.lua source <afile> | PackerCompile
   augroup end
 ]])
+
+-- open chadtree on startup
 vim.cmd([[
   autocmd VimEnter * CHADopen
 ]])
+
