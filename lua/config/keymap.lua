a17c830a4505d26042807b142623bdbcdb816b2f
miampf <miampf@proton.me>
2023-12-29 13:17:24 +0100

    changed required keys for find_file

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -40,7 +40,7 @@ map("n", "<Leader>vl", ":VimtexClean<CR>", { desc = "Clean the local files" })
 
 -- telescope bindings
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<Leader>FF', builtin.find_files, {})
+vim.keymap.set('n', '<Leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<Leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<Leader>fh', builtin.help_tags, {})
